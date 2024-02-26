92fe16a87dbf6ecfccf693e33e2b7c0980df0956
Saksham Tomer <132002655+saksham-tomer@users.noreply.github.com>
2024-02-26 11:52:08 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  
   images: {
-    loader: "custom",
-    loaderFile: "./loader.js",
+   
     remotePatterns: [
       {
         protocol: "https",
